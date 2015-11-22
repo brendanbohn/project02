2abd3f65bb8f73e9c9a59bf071ec8b69adcbb321
Eric Rogers <eric@nookzy.com>
2015-11-22 14:11:26 -0800

    paperclip added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
 	has_many :children
   has_one :questionnaire
 
+  has_attached_file :avatar,
+                    :styles => { :medium => "150x150>", :thumb => "44x44#" },
+                    :default_url => "/images/:style/missing.png"
+
+  validates_attachment :avatar, :presence => true,
+                       :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
+                       :size => { :in => 0..1000.kilobytes }
+
   extend FriendlyId
   friendly_id :username, use: :slugged
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120001621) do
+ActiveRecord::Schema.define(version: 20151121025850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,10 +81,14 @@ ActiveRecord::Schema.define(version: 20151120001621) do
     t.string   "email"
     t.text     "bio"
     t.string   "username"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "image"
     t.string   "slug"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
