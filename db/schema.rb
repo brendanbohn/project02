efef684d28b85cb11632ed5fbd4e510973fb4013
brendanbohn <brendanbohn@comcast.net>
2015-11-22 13:08:17 -0800

    created mission-statement column for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121024310) do
+ActiveRecord::Schema.define(version: 20151122210615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,10 +88,11 @@ ActiveRecord::Schema.define(version: 20151121024310) do
     t.string   "email"
     t.text     "bio"
     t.string   "username"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "image"
     t.string   "slug"
+    t.text     "mission_statement"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
