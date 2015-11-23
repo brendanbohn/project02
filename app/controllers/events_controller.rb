ebfd72f9daf5f9ea1f7b9832f3f7e99f8935616c
brendanbohn <brendanbohn@comcast.net>
2015-11-23 12:03:20 -0800

    events showing on page properly. still need to fix redirect path though on event create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,7 +8,8 @@ class EventsController < ApplicationController
 	def create
 		# @group = Group.find(params[:id])
 		event_params = params.require(:event).permit(:title, :description, :date, :group_id)
-		@event = Event.create(event_params)
+		@event = Event.new(event_params)
+		# CHANGE THIS TO REDIRECT BACK TO THE GROUP PAGE
 		redirect_to root_path
 	end
 
