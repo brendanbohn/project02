d2a0c6a997e79f12b2d063019738969f2dabd9eb
BreanaMarie <msbremarie@gmail.com>
2015-11-22 23:07:36 -0800

    convert score into star image. display compatability score of current user to user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   def compat userA, userB
     # take current user, compaire questionnaire results to all other users 
     #score how many points
+    #do not run against self
     if (userA != userB)
       score =0
       if userA.questionnaire.question1 == userB.questionnaire.question1
@@ -22,6 +23,23 @@ module ApplicationHelper
       end
     
     return score
+    end
+  end
+
+  def score_star score
+    puts case score
+    when 1
+      return image_tag "star.png"
+    when 2
+      return (image_tag "star.png")+(image_tag "star.png")
+    when 3
+      return (image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")
+    when 4
+      return (image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")
+    when 5
+      return (image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")+(image_tag "star.png")
+    else
+      return "there is no combatability"
     end
   end
 
